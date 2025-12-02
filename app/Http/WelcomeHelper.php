4fe0f524a5d534b8e1f3354e31b9f1b3a784b029
andrijanikic23 <andrijanikic001@gmail.com>
2025-12-02 14:45:18 +0100

    * Welcome helper and welcome blade

---
+++ app/Http/WelcomeHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http;
+
+class WelcomeHelper
+{
+
+    const PROPERTIES = [
+        "apartment" => "stan",
+        "house" => "kuća",
+        "commercial space" => "poslovni prostor",
+        "construction land" => "građevinsko zemljište",
+        "agricultural land" => "poljoprivredno zemljište",
+        "holiday cottage" => "vikendica",
+        "warehouse" => "magacin",
+    ];
+
+    public static function propertyType($property)
+    {
+        foreach(self::PROPERTIES as $english => $serbian)
+        {
+            if($property == $english)
+            {
+                return $serbian;
+            }
+        }
+    }
+
+    public static function amenities($parking, $furnished)
+    {
+        if($parking == 1 && $furnished == 1)
+        {
+            return "parking | namešten";
+        }
+        elseif ($parking == 1 && $furnished == 0)
+        {
+            return "parking";
+        }
+        elseif ($parking == 0 && $furnished == 1)
+        {
+            return "namešten";
+        }
+    }
+}
