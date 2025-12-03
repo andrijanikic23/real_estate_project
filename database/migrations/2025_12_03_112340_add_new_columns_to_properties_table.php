7002cdca0ba004fba9defd26e5472501ac5ce54e
andrijanikic23 <andrijanikic001@gmail.com>
2025-12-03 12:34:43 +0100

    * Adding bedroom number and commercial type columns

---
+++ database/migrations/2025_12_03_112340_add_new_columns_to_properties_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->string('bedroom_number')->after('area')->nullable();
+            $table->string('commercial_type')->after('bedroom_number')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->dropColumn('bedroom_number', 'commercial_type');
+        });
+    }
+};
