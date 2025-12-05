99a8546d019fc8aa57648f2959ec28aae35d4716
andrijanikic23 <andrijanikic001@gmail.com>
2025-12-05 19:00:45 +0100

    * Navigation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,11 +16,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Elms+Sans:ital,wght@0,100..900;1,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     @yield("head")
+    @include("navigation")
 </head>
 <body>
 
 
     @yield("content")
+
+
 
 </body>
 </html>
---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,67 @@
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #9adaed;">
+    <div class="container-fluid d-flex align-items-center">
+
+        <!-- Logo -->
+        <a class="navbar-brand d-flex align-items-center me-1" href="{{ route('properties.index') }}">
+            <img src="{{ asset('/images/logo_nekretnine.png') }}" alt="Logo" width="60" height="60">
+        </a>
+
+
+        <!-- Collapse (meni) - odmah posle logotipa -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 d-flex flex-row align-items-center">
+                <li class="nav-item dropdown me-2">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                        Prodaja
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Akcija</a></li>
+                        <li><a class="dropdown-item" href="#">Još jedna akcija</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Nešto drugo</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown me-2">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                        Izdavanje
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Akcija</a></li>
+                        <li><a class="dropdown-item" href="#">Još jedna akcija</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Nešto drugo</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item me-2">
+                    <a href="{{ route('properties.create') }}" class="nav-link" tabindex="-1" aria-disabled="true">Novi oglas</a>
+                </li>
+
+                <li class="nav-item me-2">
+                    <a href="{{ route('properties.create') }}" class="nav-link" tabindex="-1" aria-disabled="true">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 d-flex align-items-center">
+                <li class="nav-item me-2 p-2 bd-highlight">
+                    <a href="" class="nav-link" tabindex="-1" aria-disabled="true"><i class="fa-solid fa-bookmark"></i></a>
+                </li>
+                <li>
+                <li class="nav-item me-2 p-2 bd-highlight">
+                    <a href="{{ route('profile.edit') }}" class="nav-link" tabindex="-1" aria-disabled="true"><i class="fa-solid fa-user"></i></a>
+                </li>
+                </li>
+            </ul>
+        </div>
+
+
+
+        <!-- Hamburger (stavljen na kraj i pomeren desno) -->
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    </div>
+</nav>
