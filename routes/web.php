dca2344b9c162849fe9158a92c6afeb0e456105b
andrijanikic23 <andrijanikic001@gmail.com>
2025-12-16 14:23:27 +0100

    * Route groups and User favourite discard route

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,29 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\PropertyImageController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserFavouriteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('properties.index');
 });
 
+Route::controller(PropertyController::class)->group(function(){
+    Route::post('properties/bookmark', 'like')->name('properties.bookmark');
+    Route::get('properties/search', 'filter')->name('properties.filter');
+    Route::get('properties/posted', 'posted')->name('properties.posted')->middleware('auth');
+    Route::delete('image/delete/{image}', 'deleteImage')->name('image.delete');
+    Route::get('properties/bookmarks', 'bookmark')->name('property.favourite')->middleware('auth');
+});
 
-Route::post('contact/sent', [\App\Http\Controllers\QuestionController::class, 'sent'])->name('contact.sent');
-Route::post('properties/bookmark', [PropertyController::class, 'like'])->name('properties.bookmark');
-Route::get('properties/search', [PropertyController::class, 'filter'])->name('properties.filter');
-Route::get('properties/posted', [PropertyController::class, 'posted'])->name('properties.posted')->middleware('auth');
-Route::delete('image/delete/{image}', [PropertyImageController::class, 'deleteImage'])->name('image.delete');
-Route::view('/contact', 'contact')->name('contact');
+Route::delete('user-favourite/delete/{favourite}', [UserFavouriteController::class, 'discard'])->name('user.favourite.delete');
+
+Route::post('contact/sent', [QuestionController::class, 'sent'])->name('contact.sent');
 Route::resource('properties', PropertyController::class)
     ->middlewareFor('create', 'auth');
+
+Route::view('/contact', 'contact')->name('contact');
 
 
 
